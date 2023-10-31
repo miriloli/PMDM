74cf31cb7f5361154f84f62cac41b698f8526561
miriloli <miriam3gga@gmail.com>
2023-10-31 21:09:34 +0100

    casi lo mismo

--- app/src/main/java/com/example/myapplicationirir/MainActivity.java
+++ app/src/main/java/com/example/myapplicationirir/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("www.google.com");
+        webView.loadUrl("http://www.google.com");
 
     }
     public void comprobarConexion() {
